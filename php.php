30fe87272b8e21a7990cd7dffef5a7ec3ffcfbf5
Stephen Bryant <sebryant816@yahoo.com>
2019-05-28 21:14:11 -0400

    Update php.php

--- php.php
+++ php.php
@@ -2,10 +2,7 @@
 
 $name="Stephen Bryant";
 
-define("SERVERNAME", "localhost", true);
-define("USERNAME", "jaxcode83", true);
-define("PASSWORD", "Ducks0up", true);
-define("DBNAME", "jaxcode83", true);
+
 
 
 $stabbr = array (“AL”, 
