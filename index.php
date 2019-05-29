db6c3cda3cc471b9be72c887ea3d7b83225817e0
Stephen Bryant <sebryant816@yahoo.com>
2019-05-28 21:11:44 -0400

    Update index.php

--- index.php
+++ index.php
@@ -30,13 +30,6 @@
 
  <!--<Connection Creditials>--->   
 
-    <?
-    $servername = "localhost";
-    $username = "jaxcode83";
-    $password = "Ducks0up";
-    $dbname = "jaxcode83";
-    ?>
-
  <!--<Search field>--->
 
     <div class="probootstrap-search" id="probootstrap-search">
