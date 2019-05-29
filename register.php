d88478856fdf9ef63e625877e8dc6d25ef2ef696
Stephen Bryant <sebryant816@yahoo.com>
2019-05-28 21:14:55 -0400

    Update register.php

--- register.php
+++ register.php
@@ -32,12 +32,6 @@
 
  <!--<Connection Creditials>--->   
 
-    <?
-    $servername = "localhost";
-    $username = "jaxcode83";
-    $password = "Ducks0up";
-    $dbname = "jaxcode83";
-    ?>
 
 
   <div class="probootstrap-search" id="probootstrap-search">
